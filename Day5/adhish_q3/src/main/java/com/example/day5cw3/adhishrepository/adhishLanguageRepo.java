9a32642a756b86c45cb12431a86752f96b38aa73
adhish <121445772+adhish274@users.noreply.github.com>
2024-04-22 10:47:49 +0530

    Delete Day5 directory

--- Day5/adhish_q3/src/main/java/com/example/day5cw3/adhishrepository/adhishLanguageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day5cw3.adhishrepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5cw3.adhishmodel.adhishLanguage;
-
-public interface adhishLanguageRepo extends JpaRepository<adhishLanguage,Integer>{
-    
-}
